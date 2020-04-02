0761d72e5716ce8f326ec358c059f891e34ed476
Gekson Cinaty <gekson.cinaty@indt.org.br>
2020-04-01 23:16:07 -0300

    mergesort with List

--- src/algorithmsandconstructs/s2019211/Feed.java
+++ src/algorithmsandconstructs/s2019211/Feed.java
@@ -7,7 +7,6 @@ import java.util.*;
 
 public class Feed implements FeedInterface {
     private List<FeedItem> feedList;
-    private FeedItem[] feedListArray;
 
     public Feed() {
         feedList = new ArrayList<>();
@@ -20,10 +19,9 @@ public class Feed implements FeedInterface {
 
     @Override
     public Collection<String> listTitles() {
-        feedListArray = feedList.toArray(new FeedItem[feedList.size()]);
-        mergeSortFeedItem(feedListArray);
+        mergeSortFeedItem(feedList);
         List<String> list = new ArrayList<>();
-        for (FeedItem feedItem : feedListArray) {
+        for (FeedItem feedItem : feedList) {
             list.add(feedItem.getTitle());
         }
         return list;
@@ -33,7 +31,7 @@ public class Feed implements FeedInterface {
     public FeedItem getItem(String title) {
         String[] titles = listTitles().toArray(new String[0]);
         int item = binarySearch(titles, 0, titles.length - 1, title);
-        return item >= 0 ? feedListArray[item] : null;
+        return item >= 0 ? feedList.get(item) : null;
     }
 
     @Override
@@ -47,9 +45,7 @@ public class Feed implements FeedInterface {
         Collection<String> list = listTitles();
         for (String title: list) {
             String[] keys = title.split(" ");
-            List<String> listKeys = Arrays.asList(keys);
-            mergeSortString(listKeys);
-//            keys = quickSort(listKeys).toArray(new String[list.size()]);
+            mergeSortString(keys);
             for (String key: keys){
                 int item = binarySearch(keys, 0, keys.length - 1, keyword);
                 if(item >= 0) {
@@ -74,73 +70,29 @@ public class Feed implements FeedInterface {
             return binarySearch(titles, mid +1, high, key);
     }
 
-    private List<String> quickSort(List<String> input){
-
-        if(input.size() <= 1){
-            return input;
-        }
-
-        int middle = (int) Math.ceil((double)input.size() / 2);
-        String pivot = input.get(middle);
-
-        List<String> less = new ArrayList<String>();
-        List<String> greater = new ArrayList<String>();
-
-        for (int i = 0; i < input.size(); i++) {
-            if(input.get(i).compareTo(pivot) <= 0){
-                if(i == middle){
-                    continue;
-                }
-                less.add(input.get(i));
-            }
-            else{
-                greater.add(input.get(i));
-            }
-        }
-
-        return concatenate(quickSort(less), pivot, quickSort(greater));
-    }
-
-    private List<String> concatenate(List<String> less, String pivot, List<String> greater){
-
-        List<String> list = new ArrayList<String>();
-
-        for (int i = 0; i < less.size(); i++) {
-            list.add(less.get(i));
-        }
-
-        list.add(pivot);
-
-        for (int i = 0; i < greater.size(); i++) {
-            list.add(greater.get(i));
-        }
-
-        return list;
-    }
-
-    private void mergeSortFeedItem(FeedItem[] array) {
+    private void mergeSortString(String[] array) {
 
         if (array.length > 1) {
 
             int firstHalfSize = array.length / 2;
-            FeedItem[] firstHalf = new FeedItem[firstHalfSize];
+            String[] firstHalf = new String[firstHalfSize];
             System.arraycopy(array, 0, firstHalf, 0, firstHalfSize);
-            mergeSortFeedItem(firstHalf);
+            mergeSortString(firstHalf);
 
-            mergeSortFeedItem(firstHalf);
+            mergeSortString(firstHalf);
 
 
             int secondHalfSize = array.length - firstHalfSize;
-            FeedItem[] secondHalf = new FeedItem[secondHalfSize];
+            String[] secondHalf = new String[secondHalfSize];
             System.arraycopy(array, firstHalfSize, secondHalf, 0, secondHalfSize);
-            mergeSortFeedItem(secondHalf);
+            mergeSortString(secondHalf);
 
             merge(firstHalf, secondHalf, array);
 
         }
     }
 
-    public void merge(FeedItem[] a, FeedItem[] b, FeedItem[] s) {
+    public void merge(String[] a, String[] b, String[] s) {
 
         int counterA = 0;
         int counterB = 0;
@@ -148,7 +100,7 @@ public class Feed implements FeedInterface {
 
         while(counterA < a.length && counterB < b.length) {
 
-            if(a[counterA].getTitle().compareTo(b[counterB].getTitle()) < 0) {
+            if(a[counterA].compareTo(b[counterB]) < 0) {
                 s[counterS] = a[counterA];
                 counterA++;
             }
@@ -172,32 +124,32 @@ public class Feed implements FeedInterface {
         }
     }
 
-    private void mergeSortString(List<String> array) {
+    private void mergeSortFeedItem(List<FeedItem> array) {
 
         if (array.size() > 1) {
 
             int firstHalfSize = array.size() / 2;
-            List<String> firstHalf = new ArrayList<String>();
-            String[] firstHalfAux = new String[firstHalfSize];
-            System.arraycopy(array.toArray(new String[0]), 0, firstHalfAux, 0, firstHalfSize);
+            List<FeedItem> firstHalf = new ArrayList<FeedItem>();
+            FeedItem[] firstHalfAux = new FeedItem[firstHalfSize];
+            System.arraycopy(array.toArray(new FeedItem[0]), 0, firstHalfAux, 0, firstHalfSize);
             firstHalf.addAll(Arrays.asList(firstHalfAux));
 
-            mergeSortString(firstHalf);
+            mergeSortFeedItem(firstHalf);
 
 
             int secondHalfSize = array.size() - firstHalfSize;
-            String[] secondHalfAux = new String[secondHalfSize];
-            List<String> secondHalf = new ArrayList<String>();
-            System.arraycopy(array.toArray(new String[0]), firstHalfSize, secondHalfAux, 0, secondHalfSize);
+            FeedItem[] secondHalfAux = new FeedItem[secondHalfSize];
+            List<FeedItem> secondHalf = new ArrayList<FeedItem>();
+            System.arraycopy(array.toArray(new FeedItem[0]), firstHalfSize, secondHalfAux, 0, secondHalfSize);
             secondHalf.addAll(Arrays.asList(secondHalfAux));
-            mergeSortString(secondHalf);
+            mergeSortFeedItem(secondHalf);
 
             merge(firstHalf, secondHalf, array);
 
         }
     }
 
-    public void merge(List<String> a, List<String> b, List<String> s) {
+    public void merge(List<FeedItem> a, List<FeedItem> b, List<FeedItem> s) {
 
         int counterA = 0;
         int counterB = 0;
@@ -205,8 +157,7 @@ public class Feed implements FeedInterface {
 
         while(counterA < a.size() && counterB < b.size()) {
 
-//            if(a[counterA].getTitle().compareTo(b[counterB].getTitle()) < 0) {
-            if(a.get(counterA).compareTo(b.get(counterB)) < 0) {
+            if(a.get(counterA).getTitle().compareTo(b.get(counterB).getTitle()) < 0) {
                 s.set(counterS, a.get(counterA));
                 counterA++;
             }
